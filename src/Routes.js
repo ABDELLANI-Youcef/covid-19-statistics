5da22c5953c51f1be1af026308256147e03b03f9
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-12 18:25:37 +0100

    add react-router-dom package and Routes file to the project

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from './App';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
