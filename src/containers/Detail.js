d6e50132098bf7cd0d20dfd3a405ce90d34a4a3a
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-13 16:13:38 +0100

    Show country desease informations in the details pages

--- src/containers/Detail.js
+++ src/containers/Detail.js
@@ -1,17 +1,57 @@
 import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
 
-const Detail = ({ location }) => (
-  <div>
-    <p>
-      Hello world in our application! this is a Detail page concerning
-      {' '}
-      {location.state.country}
-    </p>
-  </div>
-);
+const Detail = ({ location, data }) => {
+  const { country } = location.state;
+  const countryData = data[country].All;
+  return (
+    <>
+      <p>
+        Hello world in our application! this is a Detail page concerning
+        {' '}
+        {country}
+      </p>
+      <ul>
+        <li>
+          The capital:
+          {' '}
+          {countryData.capital_city}
+        </li>
+        <li>
+          Population:
+          {' '}
+          {countryData.population}
+        </li>
+        <li>
+          confirmed:
+          {' '}
+          {countryData.confirmed}
+          {' '}
+          confirmed case
+        </li>
+        <li>
+          {countryData.deaths}
+          {' '}
+          deaths
+        </li>
+        <li>
+          {countryData.recovered}
+          {' '}
+          recovered persons
+        </li>
+      </ul>
+    </>
+  );
+};
 
 Detail.propTypes = {
   location: PropTypes.objectOf(PropTypes.any).isRequired,
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 
-export default Detail;
+const mapStateToProps = state => ({
+  data: state.cases,
+});
+
+export default connect(mapStateToProps)(Detail);
