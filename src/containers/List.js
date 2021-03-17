9c9456499e67bce0c0be821b3380c79dadaa9caf
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-03-17 21:36:08 +0100

    add pagination to home page without the style

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import Loader from 'react-loader-spinner';
+import ReactPaginate from 'react-paginate';
 import CountryItem from '../components/CountryItem';
 import FilterForm from '../components/FilterForm';
 import GlobalInformations from '../components/GlobalInformations';
@@ -30,8 +31,13 @@ const List = ({
     return filterResults(c, filter);
   }).map(e => e[0]);
 
+  const [page, setPage] = useState(0);
+  const handlePageClick = data => {
+    const { selected } = data;
+    setPage(selected);
+  };
   if (countries.length > 0) {
-    countries = countries.filter(e => e !== 'Global').map(place => <CountryItem key={place} country={cases[place].All} name={place} />);
+    countries = countries.filter(e => e !== 'Global').slice(10 * page, 10 * page + 20).map(place => <CountryItem key={place} country={cases[place].All} name={place} />);
     countries = (
       <div className={styles.list}>
         {countries}
@@ -46,9 +52,25 @@ const List = ({
   }
   const world = Object.keys(cases).length > 0 ? <GlobalInformations world={cases.Global.All} />
     : null;
+
   return (
     <>
+      {page}
       <FilterForm filter={filter} clickHandle={clickHandle} />
+      <ReactPaginate
+        previousLabel="previous"
+        nextLabel="next"
+        breakLabel="..."
+        breakClassName="break-me"
+        pageCount={20}
+        initialPage={0}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName="pagination"
+        subContainerClassName="pages pagination"
+        activeClassName="active"
+      />
       {world}
       {countries}
     </>
